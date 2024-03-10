a86bd74b1ad2fe489368eb8787823238752deac7
alonkeyval <alon@keyval.dev>
2024-03-10 13:40:33 +0200

    chore: wip

--- frontend/webapp/app/(overview)/overview/page.tsx
+++ frontend/webapp/app/(overview)/overview/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 import React from 'react';
-import { OverviewHeader } from '@/components/overview';
-import { OVERVIEW } from '@/utils/constants';
-import { OverviewContainer } from '@/containers/overview';
+import { OVERVIEW } from '@/utils';
+import { OverviewHeader } from '@/components';
+import { OverviewContainer } from '@/containers';
 
 export default function OverviewPage() {
   return (
